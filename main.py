b5ff1902b11a3621290d7c2f95181a2fb7f0d3de
GameTec_live <benedikt.werner08@gmail.com>
2024-11-20 19:52:31 +0100

    fix: add directory check to update cmd

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ async def ping(interaction: discord.Interaction):
 @commands.has_permissions(administrator=True)
 async def update(interaction: discord.Interaction):
     await interaction.response.send_message("Updating bot...", ephemeral=False)
+    if "testbot" in os.getcwd():
+        os.chdir("..")
     os.system("git config --global --add safe.directory '*'") # just dont do this to your dev machine
     os.system("git pull")
     await interaction.followup.send("Bot updated. Restarting...", ephemeral=False)
